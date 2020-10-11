6588cbea274eb3d89e28c8921ab809dca4ac89e0
KaizerBh <kaiserbh@protonmail.com>
2020-10-12 10:35:01 +1100

    Task 1 completed

--- src/task1/SampleController.java
+++ src/task1/SampleController.java
@@ -1,9 +1,13 @@
 package task1;
 
 import java.net.URL;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
-import java.util.concurrent.TimeUnit;
 
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
@@ -66,45 +70,59 @@ public class SampleController implements Initializable {
 	 * when view button is pressed this method will execute.
 	 */
 	public void executeViewQuery() {
-		int id = Integer.parseInt(idTextField.getText());
-		String query = "SELECT * FROM staff WHERE ID=" + id;
-		Statement statement = null;
-		ResultSet results = null;
-		try {
-			statement = connection.prepareStatement(query);
-			results = statement.executeQuery(query);
+		if (isNumeric(idTextField.getText())) {
+			int id = Integer.parseInt(idTextField.getText());
+			String query = "SELECT * FROM staff WHERE ID=" + id;
+			Statement statement = null;
+			ResultSet results = null;
+			try {
+				statement = connection.prepareStatement(query);
+				results = statement.executeQuery(query);
 
-			if (results.next()) {
-				informationLabel.setText("RECORD FOUND!!!");
+				if (results.next()) {
+					informationLabel.setText("RECORD FOUND!!!");
+					idTextField.setEditable(false);
+					idTextField.setText(results.getString("id"));
+					firstNameTextField.setText(results.getString("first_name"));
+					lastNameTextField.setText(results.getString("last_name"));
+					initialTextField.setText(results.getString("initial"));
+					addressTextField.setText(results.getString("address"));
+					cityTextField.setText(results.getString("city"));
+					stateTextField.setText(results.getString("state"));
+					telephoneTextField.setText(results.getString("telephone"));
+				} else {
+					idTextField.setEditable(true);
+					informationLabel.setText("RECORD NOT FOUND!!!");
+					idTextField.setText("");
+					firstNameTextField.setText("");
+					lastNameTextField.setText("");
+					initialTextField.setText("");
+					addressTextField.setText("");
+					cityTextField.setText("");
+					stateTextField.setText("");
+					telephoneTextField.setText("");
 
-				idTextField.setText(results.getString("id"));
-				firstNameTextField.setText(results.getString("first_name"));
-				lastNameTextField.setText(results.getString("last_name"));
-				initialTextField.setText(results.getString("initial"));
-				addressTextField.setText(results.getString("address"));
-				cityTextField.setText(results.getString("city"));
-				stateTextField.setText(results.getString("state"));
-				telephoneTextField.setText(results.getString("telephone"));
-			} else {
-				informationLabel.setText("RECORD NOT FOUND!!!");
-				idTextField.setText("");
-				firstNameTextField.setText("");
-				lastNameTextField.setText("");
-				initialTextField.setText("");
-				addressTextField.setText("");
-				cityTextField.setText("");
-				stateTextField.setText("");
-				telephoneTextField.setText("");
+				}
+
+			} catch (SQLException sql) {
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setContentText(sql.getMessage());
+				alert.show();
 			}
-
-		} catch (SQLException sql) {
-			Alert alert = new Alert(AlertType.INFORMATION);
-			alert.setContentText(sql.getMessage());
+		} else {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setContentText("Please make sure ID is numeric only");
 			alert.show();
 		}
+
 	}
 
+	/**
+	 * Will clear the fields if there is data in it otherwise it will prompt a
+	 * message and ask user to explore more of the program
+	 */
 	public void executeClearButton() {
+		idTextField.setEditable(true);
 		String id = idTextField.getText();
 		String firstName = firstNameTextField.getText();
 		String lastName = lastNameTextField.getText();
@@ -118,10 +136,10 @@ public class SampleController implements Initializable {
 				&& city.isEmpty() && state.isEmpty() && telephone.isEmpty()) {
 			informationLabel.setText("Feel free to play around with Insert and View button");
 			Alert alert = new Alert(AlertType.INFORMATION);
-			alert.setContentText(
-					"Nothing to clear, Feel Free to view or insert users to the database");
+			alert.setContentText("Nothing to clear, Feel Free to view or insert users to the database");
 			alert.showAndWait();
-		}else {
+		} else {
+			idTextField.setEditable(true);
 			informationLabel.setText("Cleared succefully");
 			idTextField.setText("");
 			firstNameTextField.setText("");
@@ -132,6 +150,153 @@ public class SampleController implements Initializable {
 			stateTextField.setText("");
 			telephoneTextField.setText("");
 		}
+	}
+
+	/**
+	 * updates query also check if telephone is numeric as well not empty.
+	 */
+	public void executeUpdateQuery() {
+		idTextField.setEditable(false);
+		int id = Integer.parseInt(idTextField.getText());
+		String firstName = firstNameTextField.getText();
+		String lastName = lastNameTextField.getText();
+		String initial = initialTextField.getText();
+		String address = addressTextField.getText();
+		String city = cityTextField.getText();
+		String state = stateTextField.getText();
+		String telephone = telephoneTextField.getText();
+
+		if (firstName.isBlank() || lastName.isBlank() || initial.isBlank() || address.isBlank() || city.isBlank()
+				|| state.isBlank() || telephone.isBlank()) {
+			informationLabel.setText("RECORD FAILED TO UPDATE");
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setContentText("Please make sure that all the fields are filled and not blank");
+			alert.show();
+		} else if (!isNumeric(telephone)) {
+			informationLabel.setText("RECORD FAILED TO UPDATE");
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setContentText("Please make sure telephone is Numeric only");
+			alert.show();
+		} else if (state.length() > 3 || state.length() < 2) {
+			informationLabel.setText("RECORD FAILED TO UPDATE");
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setContentText("State can only be NSW,VIC,QLD,WA,ACT,TAS,NT");
+			alert.show();
+		} else {
+			try {
+				PreparedStatement statement = connection.prepareStatement("UPDATE staff SET first_name=?, last_name=?,"
+						+ "initial=?, address=?,city=?, state=?, telephone=? WHERE id = ?");
+				statement.setString(1, firstName);
+				statement.setString(2, lastName);
+				statement.setString(3, initial);
+				statement.setString(4, address);
+				statement.setString(5, city);
+				statement.setString(6, state);
+				statement.setString(7, telephone);
+				statement.setInt(8, id);
+
+				statement.executeUpdate();
+				informationLabel.setText("RECORD UPDATED SUCCESSFULLY");
+			} catch (SQLException sql) {
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setContentText(sql.getMessage());
+				alert.show();
+				informationLabel.setText("RECORD FAILED TO UPDATE");
+			}
+		}
+
+	}
+
+	/**
+	 * Will insert data into database
+	 */
+	public void executeInsertQuery() {
+		idTextField.setEditable(false);
+		String id = idTextField.getText();
+		String firstName = firstNameTextField.getText();
+		String lastName = lastNameTextField.getText();
+		String initial = initialTextField.getText();
+		String address = addressTextField.getText();
+		String city = cityTextField.getText();
+		String state = stateTextField.getText();
+		String telephone = telephoneTextField.getText();
+
+		if (firstName.isBlank() || lastName.isBlank() || initial.isBlank() || address.isBlank() || city.isBlank()
+				|| state.isBlank() || telephone.isBlank()) {
+			informationLabel.setText("FAILED TO INSERT RECORD");
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setContentText("Please make sure that all the fields are filled and not blank except id field");
+			alert.show();
+		} else if (!isNumeric(telephone)) {
+			informationLabel.setText("FAILED TO INSERT RECORD");
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setContentText("Please make sure telephone is Numeric only");
+			alert.show();
+		} else if (state.length() > 3 || state.length() < 2) {
+			informationLabel.setText("FAILED TO INSERT RECORD");
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setContentText("State can only be NSW,VIC,QLD,WA,ACT,TAS,NT");
+			alert.show();
+		} else {
+			if (id.isEmpty()) {
+				String insertStaff = ("INSERT INTO staff (last_name, first_name, initial, address, city, state, telephone) VALUES('"
+						+ lastName + "', '" + firstName + "', " + "'" + initial + "', '" + address + "', '" + city
+						+ "', '" + state + "', '" + telephone + "')");
+				try {
+					Statement statement = connection.prepareStatement(insertStaff);
+					statement.execute(insertStaff);
+					String query = "SELECT id FROM staff ORDER BY id DESC LIMIT 1";
+					Statement updateID = connection.prepareStatement(query);
+					ResultSet rset = updateID.executeQuery(query);
+					while (rset.next()) {
+						idTextField.setText(rset.getString(1));
+					}
+					idTextField.setEditable(false);
+					informationLabel.setText("SUCCESFULLY INSERTED NEW RECORD");
+				} catch (SQLException sql) {
+					informationLabel.setText("FAILED TO INSERT RECORD");
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setContentText(sql.getMessage());
+					alert.show();
+				}
+			} else {
+				informationLabel.setText("ID FIELD MUST BE EMPTY");
+				idTextField.setText("");
+			}
+		}
+
+	}
+	
+	/**
+	 * Will lock the id field on key pressed
+	 */
+	public void lockIDFieldOnKeyPressed() {
+		idTextField.setEditable(false);
+	}
+	
+	/**
+	 * Will lock the id field on key pressed
+	 */
+	public void unlockIDFieldOnHover() {
+		idTextField.setEditable(true);
+	}
+	
+
+	/**
+	 * 
+	 * @param strNum String character to be checked
+	 * @return Boolean if text is numeric or not
+	 */
+	public static boolean isNumeric(String strNum) {
+		if (strNum == null) {
+			return false;
+		}
+		try {
+			double d = Double.parseDouble(strNum);
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+		return true;
 	}
 
 }
